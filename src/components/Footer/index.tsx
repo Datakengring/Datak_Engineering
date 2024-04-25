e301f7a00a5cbbae9476891285765d738caaec01
Prime Senpai <Bashfordbash@gmail.com>
2024-04-25 21:38:17 +0100

    feat: Home page first hero section complete with responsiveness

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -9,7 +9,7 @@ import {
 const Footer = () => {
   return (
     <div className="w-[100%] text-[#797B89] flex flex-col items-center text-[0.875rem]">
-      <div className="flex py-[2rem] px-[4rem] gap-[5rem] justify-between">
+      <div className="flex flex-col lg:flex-row py-[2rem] px-[1.5rem] lg:px-[4rem] lg:gap-[5rem] gap-[2rem] justify-between">
         <div className="flex-grow">
           <NavLink to={"/"}>
             <img src={logo} className="h-[2rem]" alt="logo" />
@@ -19,48 +19,53 @@ const Footer = () => {
             drive success andsustainability in the oil and gas sector
           </p>
         </div>
-        <div className="w-[12rem] flex flex-col gap-2">
-          <h2 className="text-[#383A47] text-[1rem] font-[700]">Company</h2>
-          <NavLink className="hover:underline" to={"about-us"}>
-            About Us
-          </NavLink>
-          <NavLink className="hover:underline" to={"what-we-do"}>
-            What we do
-          </NavLink>
-          <NavLink className="hover:underline" to={"process-equipment-design"}>
-            Process equipment design
-          </NavLink>
-          <NavLink className="hover:underline" to={"contact-us"}>
-            Contact Us
-          </NavLink>
-        </div>
-        <div className="w-[13rem] flex flex-col gap-2 text-black">
-          <div className="flex items-center text-end justify-end gap-4">
-            <a href="">
-              <PiFacebookLogoFill
-                style={{ color: "#080051", fontSize: "1.8rem" }}
-              />
-            </a>
-            <a href="">
-              <PiTwitterLogoFill
-                style={{ color: "#080051", fontSize: "1.8rem" }}
-              />
-            </a>
-            <a href="">
-              <PiInstagramLogoFill
-                style={{ color: "#080051", fontSize: "1.8rem" }}
-              />
-            </a>
+        <div className="flex">
+          <div className="w-[12rem] flex flex-col gap-2">
+            <h2 className="text-[#383A47] text-[1rem] font-[700]">Company</h2>
+            <NavLink className="hover:underline" to={"about-us"}>
+              About Us
+            </NavLink>
+            <NavLink className="hover:underline" to={"what-we-do"}>
+              What we do
+            </NavLink>
+            <NavLink
+              className="hover:underline"
+              to={"process-equipment-design"}
+            >
+              Process equipment design
+            </NavLink>
+            <NavLink className="hover:underline" to={"contact-us"}>
+              Contact Us
+            </NavLink>
           </div>
-          <p className="leading-[1.1rem] text-end">
-            41 Voyage Close Kaura District Abuja Nigeria.
-          </p>
-          <p className=" text-end">
-            <a href="mailto:contact@datak.ng">contact@datak.ng</a>
-          </p>
-          <p className=" text-end">
-            <a href="tel:+234 814 800 2982">+234 814 800 2982</a>
-          </p>
+          <div className="w-[13rem] flex flex-col gap-2 text-black">
+            <div className="flex items-center text-end justify-end gap-4">
+              <a href="">
+                <PiFacebookLogoFill
+                  style={{ color: "#080051", fontSize: "1.8rem" }}
+                />
+              </a>
+              <a href="">
+                <PiTwitterLogoFill
+                  style={{ color: "#080051", fontSize: "1.8rem" }}
+                />
+              </a>
+              <a href="">
+                <PiInstagramLogoFill
+                  style={{ color: "#080051", fontSize: "1.8rem" }}
+                />
+              </a>
+            </div>
+            <p className="leading-[1.1rem] text-end">
+              41 Voyage Close Kaura District Abuja Nigeria.
+            </p>
+            <p className=" text-end">
+              <a href="mailto:contact@datak.ng">contact@datak.ng</a>
+            </p>
+            <p className=" text-end">
+              <a href="tel:+234 814 800 2982">+234 814 800 2982</a>
+            </p>
+          </div>
         </div>
       </div>
       <div className="w-[80%] text-center h-[3rem] flex justify-center items-center border-t-[2px] border-solid border-[#F8F8FA]">
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,27 +6,27 @@ import SecondaryButton from "../../components/Buttons/SecondaryButton";
 const HomePage = () => {
   return (
     <MainWrapper>
-      <div className="bg-secondary px-[5rem] py-[5rem] flex">
-        <div className="w-[50%]">
-          <h2 className="bg-gradient-to-r from-linearOne to-linearTwo inline-block text-transparent bg-clip-text text-[3rem] font-[600]">
+      <div className="bg-secondary px-[1.5rem] lg:px-[5rem] py-[3rem] lg:py-[5rem] flex flex-col gap-[3rem] lg:gap-0 lg:flex-row">
+        <div className="w-[100%] lg:w-[50%]">
+          <h2 className="bg-gradient-to-r from-linearOne to-linearTwo inline-block text-transparent bg-clip-text text-[2.4rem] lg:text-[3rem] font-[600]">
             Precision Engineering
             <br />
-            <span className="text-textColorPrimary ">
+            <span className="text-textColorPrimary font-[500]">
               for the Dynamic Oil and Gas Sector
             </span>
           </h2>
-          <p className="mt-[2rem] w-[75%]">
+          <p className="mt-[1.5rem] lg:mt-[2rem] lg:w-[75%]">
             At DATAK Engineering Service Limited, we are your trusted partner
-            for comprehensive engineeringsolutions tailored to meet the diverse
+            for comprehensive engineering solutions tailored to meet the diverse
             needs of the oil and gas industry.
           </p>
-          <div className="mt-[3rem] font-medium flex gap-6">
+          <div className="mt-[2rem] lg:mt-[3rem] font-medium flex gap-6">
             <PrimaryButton path="contact-us">Contact our team</PrimaryButton>
             <SecondaryButton path={"what-we-do"}>What we do</SecondaryButton>
           </div>
         </div>
-        <div className="w-[50%] flex justify-center">
-          <img className="h-[30rem]" src={heroOne} alt="HeroImg1" />
+        <div className="w-[100%] lg:w-[50%] flex justify-center">
+          <img className="lg:h-[30rem]" src={heroOne} alt="HeroImg1" />
         </div>
       </div>
     </MainWrapper>
