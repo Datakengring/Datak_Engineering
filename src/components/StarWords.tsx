d888fb2eae2b99b789ed84377236b86ee1e75e21
Farouk-ayo <mustaphafarouk41@gmail.com>
2024-04-28 02:46:31 +0100

    whatWedo i8n done

--- src/components/StarWords.tsx
+++ src/components/StarWords.tsx
@@ -4,7 +4,7 @@ const StarWords = ({ word }: { word: string }) => {
   return (
     <div className="flex w-fit mb-4 bg-[#EAA5080A] px-4 py-2 rounded-[1.2rem]">
       <img src={star} alt="star" />
-      <p className="text-yellow text-center">{word}</p>
+      <p className="text-yellow text-center font-medium">{word}</p>
     </div>
   );
 };
