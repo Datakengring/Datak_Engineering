c124ccc50cddbbdaa6df7fb7ae2a44476279127c
Prime Senpai <Bashfordbash@gmail.com>
2024-05-05 21:52:31 +0100

    Feat: Splashscreen and bug fix

---
+++ src/components/SplashScreen/index.tsx
@@ -0,0 +1,53 @@
+import { useEffect } from "react";
+import anime from "animejs";
+import logo from "/svgs/logo.svg";
+
+const SplashScreen = ({ finishLoading }: { finishLoading: any }) => {
+  // const [isMounted, setIsMounted] = useState(false);
+  const animate = () => {
+    const loader = anime.timeline({ complete: () => finishLoading() });
+    loader
+      .add({
+        targets: "#logo",
+        delay: 0,
+        scale: 1,
+        duration: 500,
+        easing: "easeInOutExpo",
+      })
+      .add({
+        targets: "#logo",
+        delay: 0,
+        scale: 1.1,
+        duration: 500,
+        easing: "easeInOutExpo",
+      })
+      .add({
+        targets: "#logo",
+        delay: 0,
+        scale: 1,
+        duration: 500,
+        opacity: 0.5,
+        easing: "easeInOutExpo",
+      })
+      .add({
+        targets: "#logo",
+        delay: 0,
+        scale: 6,
+        duration: 500,
+        opacity: 0,
+        easing: "easeInOutExpo",
+      });
+  };
+  useEffect(() => {
+    // const timeout = setTimeout(() => setIsMounted(true), 1000);
+    animate();
+    // return clearTimeout(timeout);
+  }, []);
+  return (
+    <div className="h-screen flex justify-center items-center ">
+      <img id="logo" src={logo} className="w-[12rem]" />
+    </div>
+  );
+};
+
+export default SplashScreen;
--- src/layouts/SuspenseLayout/index.tsx
+++ src/layouts/SuspenseLayout/index.tsx
@@ -1,6 +1,6 @@
 import { ReactElement, Suspense } from "react";
 import { Outlet } from "react-router-dom";
-import Loader from "../../components/Loader";
+import Loader from "../../components/SplashScreen/Loader";
 
 const SuspenseLayout: () => ReactElement = () => (
   <Suspense fallback={<Loader />}>
