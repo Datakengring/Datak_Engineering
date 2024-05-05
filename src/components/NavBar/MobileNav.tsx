95f47c1e96164cb24efa3bcf684253a624901bbb
Prime Senpai <Bashfordbash@gmail.com>
2024-05-05 22:06:44 +0100

    Feat: nav bg fix

--- src/components/NavBar/MobileNav.tsx
+++ src/components/NavBar/MobileNav.tsx
@@ -9,15 +9,15 @@ const MobileNav = ({ show, setShow }: IShow) => {
       <div
         className={`fixed top-0 h-[100%] transition-all duration-700 ease-in-out ${
           show
-            ? "w-[100%] right-0 bg-primary opacity-50 backdrop-blur-md transition-all duration-700 ease-in-out"
-            : "w-[100%] right-[-28rem] bg-primary opacity-50 transition-all delay-200 duration-[900ms] ease-in-out "
+            ? "w-[100%] right-0 bg-primary opacity-50 backdrop-blur-md transition-all duration-500 ease-in-out"
+            : "w-[100%] right-[-100%] bg-primary opacity-50 transition-all delay-200 duration-[900ms] ease-in-out "
         }`}
       ></div>
       <div
         className={`fixed h-[100%] top-0 z-20 lg:hidden flex flex-col items-center justify-between text-primary px-[2rem] pt-[6rem] pb-6 transition-all duration-[1000ms] ease-in-out  ${
           show
-            ? "w-[80%] right-0 z-30 delay-200 bg-[white] backdrop-blur-md duration-[1000ms] ease-in-out"
-            : "z-30 right-[-18rem] bg-white transition-all duration-[1000ms] ease-in-out"
+            ? "w-[80%] right-0 z-30 delay-400 bg-[white] backdrop-blur-md duration-[1000ms] ease-in-out"
+            : "z-30 right-[-100%] bg-white transition-all duration-[1000ms] ease-in-out"
         }`}
       >
         <div className="w-[100%] flex flex-col items-center justify-center gap-8">
